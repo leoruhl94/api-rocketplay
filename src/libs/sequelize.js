6cdfcd6549a205537e792ef2740e4b6e577349a9
leoruhl94 <leoruhl94@gmail.com>
2021-11-21 16:36:11 -0300

    initial commit

---
+++ src/libs/sequelize.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const { Sequelize } = require("sequelize");
+const { config } = require("../config/config");
+const setupModels = require("../db/models/index");
+
+const options = {
+  dialect: "postgres",
+  logging: false
+};
+if (config.isProd) {
+  options.dialectOptions = {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  };
+  options.logging = false;
+}
+
+const sequelize = new Sequelize(config.dbUrl, options);
+
+setupModels(sequelize);
+
+// sequelize.sync({ force: true });  // activar para modo desarrollo
+
+module.exports = sequelize;
