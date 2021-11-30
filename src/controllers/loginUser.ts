1b26006aa2379812b7bd3928f33ed71005ad544a
Kofrantz <franco.alfano1404@gmail.com>
2021-11-30 19:04:48 -0300

    youtube upload

--- src/controllers/loginUser.ts
+++ src/controllers/loginUser.ts
@@ -15,6 +15,43 @@ const {google} = require("googleapis")
 
 router.post('/', async (req:Request, res:Response, next:NextFunction)=> {
     try{
+        const {code} = req.body
+        console.log(code)
+        const oAuthClient = new google.auth.OAuth2(
+                OAuth2Data.web.client_id,
+                OAuth2Data.web.client_secret,
+                OAuth2Data.web.redirect_uris[0]
+        )
+        await oAuthClient.getToken(code, function(err, tokens){
+            if(err) throw err
+            console.log('successfully authenticated')
+            //console.log(tokens)
+            oAuthClient.setCredentials(tokens)
+            return res.json({
+                data: tokens
+            })
+        })
+        /* console.log('oauth: ', oAuthClient)
+        const oauth = google.oauth2({
+            version: 'v2',
+            auth: oAuthClient
+        })
+        //obtener informacion del usuario
+        oauth.userinfo.get(function(err, response){
+            //console.log(response.data.name)
+            res.json({
+                data: response
+            })
+        }) */
+
+        /* const youtube = google.youtube({
+            version: 'v3',
+            auth: oAuthClient
+        }) */
+    } catch (error) {
+        console.log(error)
+    }
+    /* try{
         const {isBusiness, name, email} = req.body
         const userBool = await Users.findOne({
             where: {mail: email}
@@ -28,7 +65,7 @@ router.post('/', async (req:Request, res:Response, next:NextFunction)=> {
             return res.status(200).json({isRegistered: false})
         }
         // Si ya estaba creado mando true
-        return res.status(200).json({isRegistered: true})
+        return res.status(200).json({isRegistered: true}) */
 
 
 
@@ -89,22 +126,24 @@ router.post('/', async (req:Request, res:Response, next:NextFunction)=> {
         // }else{
         //     res.status(400).json({message:"Empresa no registrada"})
         // }
-        }catch(error){
+        /* }catch(error){
             res.status(400).json({message:error})
-        }
+        } */
 })
 
 router.get('/',  async (req:Request, res:Response, next:NextFunction)=> {
+    
     try {
         const {email} = req.query
         const userBool = await Users.findOne({
             where: {mail: email}
         })
+
         res.status(200).json({isRegistered: !!userBool})
 
     }  catch (error) {
         res.status(400).json({message:error})
-    }
+    } 
 
 })
 
--- src/controllers/uploadVideo.ts
+++ src/controllers/uploadVideo.ts
@@ -28,89 +28,56 @@ const upload = multer({storage: storage}).single('videoFile') //el nombre del ke
 
 
 router.post('/', async (req:Request, res:Response, next:NextFunction) => {//subir un video
-  
-  // youtube.search.list({
-    //     part: 'snippet',
-    //     q: 'your search query'
-    //   }, function (err, data) {
-      //     if (err) {
-        //       res.send('Error: ' + err);
-        //     }
-        //     if (data) {
-          //       res.json(data)
-          //     }
-          //   });
-          try {
-            const oAuthClient = new google.auth.OAuth2(
-              OAuth2Data.web.client_id,
-              OAuth2Data.web.client_secret,
-              OAuth2Data.web.redirect_uris[0]
-              )
-              const {title, accessToken} = req.body
-              oAuthClient.credentials = {
-                  access_token: accessToken
-                }
-                
-                const youtube = google.youtube({
-                  version: 'v3',
-                  auth: oAuthClient
-                })
-                
-                upload(req, res, function(err){
-                  if(err) throw err
-                  console.log(req.body.title)
-  
-          // const youtube = google.youtube({ //youtube authentication
-          //     version: 'v3',
-          //     auth: oAuthClient
-          // })
-          console.log("llegue aca")
-          // let a = axios.post("https://www.googleapis.com/upload/youtube/v3/videos&key=AIzaSyCy5bFCjzTESdANJcoW8GNZRvVS6LJ2864&access_token=ya29.a0ARrdaM_0HeyVKEOYyVvJ0FMHTiy4mQu8qEd_gvPQRVyaRTeCAcwJ43v0stDfhlTdqy_haABBpaWj13Ubhb_nYZIcrnO0qqyrvu0pmv3Pdiby7IFQH2xU8r7bDJRech3aAcNq8tb7VBSqL8NLgih91V_Mdcp7", {
-          //   resource:{
-          //     snippet:{
-          //         title, //titulo
-          //         description: 'this is a test of youtube api' ///descripcion
-          //     },
-          //     status:{
-          //         privacityStatus: 'private' //video publico, privado o no listado
-          //     }
-          //   },
-          //   part:'snippet,status',
-          //   media:{
-          //       body:fs.createReadStream(req.file.path)
-          //   }
-          // }).then(r => console.log("Done !")).catch(r => console.log("Entre al catch"))
-          // res.send("asd")
-          youtube.videos.insert({//metodo para subir un video
-              resource:{
-                  snippet:{
-                      title, //titulo
-                      description: 'this is a test of youtube api' ///descripcion
-                  },
-                  status:{
-                      privacityStatus: 'private' //video publico, privado o no listado
-                  }
-              },
-              part:'snippet,status',
-              media:{
-                  body:fs.createReadStream(req.file.path)
-              }
-          },
-          (err, data) => {
-              if(err) throw err
-              console.log('uploading video done!')
+  try {
+    upload(req, res, async function(err){
+
+      const oAuthClient = new google.auth.OAuth2(
+        OAuth2Data.web.client_id,
+        OAuth2Data.web.client_secret,
+        OAuth2Data.web.redirect_uris[0]
+      )
+      const {title, tokens} = req.body
+      await oAuthClient.setCredentials(JSON.parse(tokens))     
+      if(err) throw err
       
-              fs.unlinkSync(req.file.path)//borra la copia del video de la carpeta upload
-              res.send('success!')
-          })
-        })
+      const youtube = google.youtube({
+        version: 'v3',
+        auth: oAuthClient
+      })
 
-    } catch(error) {
-      res.send(error)
-    }
-
+      youtube.videos.insert({//metodo para subir un video
+        resource:{
+          snippet:{
+            title, //titulo
+            description: 'this is a test of youtube api' ///descripcion
+          },
+          status:{
+            privacyStatus: 'private' //video publico, privado o no listado
+          }
+        },
+        part:'snippet,status',
+        media:{
+          body:fs.createReadStream(req.file.path)
+        }
+      },
+      (err, data) => {
+        if(err) throw err
+        console.log('uploading video done!')
         
+        //borra la copia del video de la carpeta upload
+        res.send('success!')
+        fs.unlink(req.file.path, err => {
+          if (err) throw err;
+        })
+        
+      })
+      
+    })
     
+              
+  } catch(error) {
+    res.send(error)
+  }     
 })
 
 module.exports = router;
