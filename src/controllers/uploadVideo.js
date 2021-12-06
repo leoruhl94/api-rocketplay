24b32ca0738e6f5b5a19566ec7958a61665f54b2
leoruhl94 <leoruhl94@gmail.com>
2021-12-06 11:54:23 -0300

    agregando aws y multers3 para subir videos

--- src/controllers/uploadVideo.js
+++ src/controllers/uploadVideo.js
@@ -4,23 +4,45 @@ const {google} = require('googleapis')
 const router = Router();
 const OAuth2Data = require('../../googleYoutube.json')
 //este archivo se descarga desde las credenciales con json
+const aws = require('aws-sdk')
 const multer = require('multer')
+const multerS3 = require('multer-s3')
 const fs = require('fs')
 
+const s3 = new aws.S3({
+  accessKeyId: 'AKIA4KENAPOS2C2TJENZ',
+  secretAccessKey: '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'
+})
+
 //modelo de la copia del video que va a crear en la carpeta upload
-// const storage = multer.diskStorage({
-//     destination: function (req, file, cb){
-//         cb(null, 'uploads')
-//     },
-//     filename: function(req, file, cb){
-//         cb(null, `${Date.now()}-${file.originalname}`)
-//     }
-// })
+const storage = multer.diskStorage({
+    destination: function (req, file, cb){
+        cb(null, 'uploads')
+    },
+    filename: function(req, file, cb){
+        cb(null, `${Date.now()}-${file.originalname}`)
+    }
+})
 //guardamos el video que me llega con multer
 // const upload = multer({storage: storage}).single('videoFile') //el nombre del key donde viene el archivo de video
 //datos del cliente
-const upload = multer({dest: 'uploads/'})
+const upload = multer({storage})
 console.log(upload)
+
+var uploadS3 = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: 'rocketplay2021',
+    acl: 'public-read',
+    metadata: function (req, file, cb) {
+      cb(null, {fieldName: file.fieldname});
+    },
+    key: function (req, file, cb) {
+      cb(null, `${Date.now()}-${file.originalname}`)
+    }
+  })
+})
+
 // Para ver mis listas de reproduccion 
 // https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&mine=true&key=AIzaSyCy5bFCjzTESdANJcoW8GNZRvVS6LJ2864&access_token=ya29.a0ARrdaM_0HeyVKEOYyVvJ0FMHTiy4mQu8qEd_gvPQRVyaRTeCAcwJ43v0stDfhlTdqy_haABBpaWj13Ubhb_nYZIcrnO0qqyrvu0pmv3Pdiby7IFQH2xU8r7bDJRech3aAcNq8tb7VBSqL8NLgih91V_Mdcp7
 
@@ -30,7 +52,6 @@ console.log(upload)
 
 router.post('/',upload.single('videoFile'), async (req, res, next) => {//subir un video
   try { //function(err){
-    upload(req, res, next)
       console.log("Afuera =>> ", req.body)
       console.log("===========================================================================================================")
       let redirectUrl = req.headers.origin;
